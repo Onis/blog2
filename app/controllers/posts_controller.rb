6d3fdeed77557f833aa4215b345d22c539b345e2
Onis <damir.s@mail.ru>
2014-04-27 17:05:28 +0600

    Create posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+
+	def index
+		@posts = Post.all
+	end
+
+	def new
+		
+	end
+
+	def create
+		@post = Post.new(posts_params)	
+
+		@post.save
+		redirect_to @post
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	private
+  def posts_params
+    params.require(:post).permit(:title, :content)
+  end
+end
